1eac8f1b3ee24443ffa353c7e72e84ae4c6df061
yangxun3 <yangxun3@lenovo.com>
2023-04-20 17:49:31 +0800

    format

--- src/views/home.vue
+++ src/views/home.vue
@@ -9,6 +9,7 @@
       <n-button type="primary">
         Primary </n-button>
       <n-button type="info">
+
         Info </n-button>
       <n-button type="success"> Success </n-button>
       <n-button type="warning"> Warning </n-button>
