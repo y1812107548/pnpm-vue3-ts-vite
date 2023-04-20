237843e7f1aef8755d54b0be5f85667c76534d7f
yangxun3 <yangxun3@lenovo.com>
2023-04-20 17:49:03 +0800

    format

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,9 +4,12 @@
     <HelloWorld :msg="msg" />
     <n-space>
       <n-button>Default</n-button>
-      <n-button type="tertiary"> Tertiary </n-button>
-      <n-button type="primary"> Primary </n-button>
-      <n-button type="info"> Info </n-button>
+      <n-button type="tertiary">
+        Tertiary </n-button>
+      <n-button type="primary">
+        Primary </n-button>
+      <n-button type="info">
+        Info </n-button>
       <n-button type="success"> Success </n-button>
       <n-button type="warning"> Warning </n-button>
       <n-button type="error"> Error </n-button>
