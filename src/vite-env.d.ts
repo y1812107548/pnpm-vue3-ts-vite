855cedd75c811eb024bda9452e3228be1dd21f79
yangxun3 <yangxun3@lenovo.com>
2023-04-21 15:05:39 +0800

    feat：新增环境配置参数

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_APP_TITLE:string
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig,loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { normalizePath } from 'vite';
 import { resolve } from 'path';
@@ -7,61 +7,64 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 const variablePath = normalizePath(resolve(__dirname, './src/style/variable.scss'));
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  optimizeDeps: {
-    force: true // 忽略缓存强制刷新
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '#': resolve(__dirname, './src/types'),
-      utils: resolve(__dirname, './src/utils'),
-      api: resolve(__dirname, './src/api')
-    }
-  },
-  server: {
-    https: false,
-    cors: true
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        {
-          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
-        }
-      ]
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()]
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "${variablePath}";`
+export default ({command,mode})=> {
+  const env: Partial<ImportMeta> = loadEnv(mode, process.cwd());
+  console.log(env);
+  return defineConfig({
+    base: './',
+    optimizeDeps: {
+      force: true // 忽略缓存强制刷新
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '#': resolve(__dirname, './src/types'),
+        utils: resolve(__dirname, './src/utils'),
+        api: resolve(__dirname, './src/api')
       }
     },
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
-    }
-  },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      // 去掉生产console和debugger
-      compress: {
-        drop_console: true,
-        drop_debugger: true
+    server: {
+      https: false,
+      cors: true
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+          {
+            'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "${variablePath}";`
+        }
+      },
+      postcss: {
+        plugins: [
+          autoprefixer({
+            overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+          })
+        ]
+      }
+    },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        // 去掉生产console和debugger
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
       }
     }
-  }
-});
+  });
+}
