b1ffcf0bdd81a05b1025f78f07f8e1a9372b3e28
yangxun3 <yangxun3@lenovo.com>
2023-04-21 17:51:41 +0800

    feat：select

--- src/views/home.vue
+++ src/views/home.vue
@@ -11,12 +11,37 @@
       <n-button type="warning"> Warning </n-button>
       <n-button type="error"> Error </n-button>
     </n-space>
+    <n-dropdown trigger="hover" :options="options" @select="handleSelect">
+      <n-button>找个地方休息</n-button>
+    </n-dropdown>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 const msg = ref<string>('vite-vue');
+const options = reactive([
+  {
+    label: '滨海湾金沙，新加坡',
+    key: 'marina bay sands',
+    disabled: true
+  },
+  {
+    label: '布朗酒店，伦敦',
+    key: 'brown\'s hotel, london'
+  },
+  {
+    label: '亚特兰蒂斯巴哈马，拿骚',
+    key: 'atlantis nahamas, nassau'
+  },
+  {
+    label: '比佛利山庄酒店，洛杉矶',
+    key: 'the beverly hills hotel, los angeles'
+  }
+]);
+const handleSelect = (key: string | number) => {
+  console.log(key);
+};
 console.log(msg);
 </script>
 
