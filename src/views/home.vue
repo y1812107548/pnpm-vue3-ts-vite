12bff02e5b786012ad65cbf1c47b3f2ee4c47cd7
yangxun3 <yangxun3@lenovo.com>
2023-04-25 15:38:14 +0800

    test:commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,8 +6,8 @@
       <n-button>Default</n-button>
       <n-button type="tertiary"> Tertiary </n-button>
       <n-button type="primary"> Primary </n-button>
-      <n-button type="info"> Info </n-button>
-      <n-button type="success"> Success </n-button>
+      <n-button type="info"> Info11 </n-button>
+      <n-button type="success"> Success222 </n-button>
       <n-button type="warning"> Warning </n-button>
       <n-button type="error"> Error </n-button>
     </n-space>
